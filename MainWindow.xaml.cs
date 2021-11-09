b8141d22fecaed5582b73b2f26ef71b520215482
Renato Luiz Carvalho <renato_luizcarvalho@hotmail.com>
2021-11-09 17:00:13 -0300

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace HoursCalculation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoThat_Click(object sender, RoutedEventArgs e)
+        {
+            var monday = TimeSpan.Parse(txtMonday.Text).TotalHours;
+            var tuesday = TimeSpan.Parse(txtTuesday.Text).TotalHours;
+            var wednesday = TimeSpan.Parse(txtWednesday.Text).TotalHours;
+            var thursday = TimeSpan.Parse(txtThursday.Text).TotalHours;
+
+            var totalHours = int.Parse(txtWeekHours.Text) - (monday + tuesday + wednesday + thursday);
+
+            txtRemainHours.Text = TimeSpan.FromHours(totalHours).ToString();
+        }
+    }
+}
